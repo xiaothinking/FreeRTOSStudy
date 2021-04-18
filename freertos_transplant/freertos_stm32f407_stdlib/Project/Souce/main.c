ea75bcd1c536fc0882730fc3ee9192cbc32a4587
xiaothinking <2681232282@qq.com>
2021-04-18 09:35:28 +0800

    FreeRTOS移植

---
+++ freertos_transplant/freertos_stm32f407_stdlib/Project/Souce/main.c
@@ -0,0 +1,152 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32F4xx_StdPeriph_Templates/main.c 
+  * @author  MCD Application Team
+  * @version V1.8.0
+  * @date    04-November-2016
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2016 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "uart.h"
+#include "delay.h"
+#include "timer.h"
+#include "led.h"
+#include "FreeRTOS.h"
+#include "task.h"
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+
+
+/************************************************
+ ALIENTEK 探索者STM32F407开发板 FreeRTOS实验2-1
+ FreeRTOS移植实验-库函数版本
+ 技术支持：www.openedv.com
+ 淘宝店铺：http://eboard.taobao.com 
+ 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+ 广州市星翼电子科技有限公司  
+ 作者：正点原子 @ALIENTEK
+************************************************/
+
+//任务优先级
+#define START_TASK_PRIO		1
+//任务堆栈大小	
+#define START_STK_SIZE 		128  
+//任务句柄
+TaskHandle_t StartTask_Handler;
+//任务函数
+void start_task(void *pvParameters);
+
+//任务优先级
+#define LED0_TASK_PRIO		2
+//任务堆栈大小	
+#define LED0_STK_SIZE 		50  
+//任务句柄
+TaskHandle_t LED0Task_Handler;
+//任务函数
+void led0_task(void *pvParameters);
+
+//任务优先级
+#define LED1_TASK_PRIO		3
+//任务堆栈大小	
+#define LED1_STK_SIZE 		50  
+//任务句柄
+TaskHandle_t LED1Task_Handler;
+//任务函数
+void led1_task(void *pvParameters);
+
+//任务优先级
+#define FLOAT_TASK_PRIO		4
+//任务堆栈大小	
+#define FLOAT_STK_SIZE 		128
+//任务句柄
+TaskHandle_t FLOATTask_Handler;
+//任务函数
+void float_task(void *pvParameters);
+
+int main(void)
+{ 
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);//设置系统中断优先级分组4
+	delay_init(168);		//初始化延时函数
+	uart_init(115200);     	//初始化串口
+	led_init();		        //初始化LED端口
+	
+	//创建开始任务
+    xTaskCreate((TaskFunction_t )start_task,            //任务函数
+                (const char*    )"start_task",          //任务名称
+                (uint16_t       )START_STK_SIZE,        //任务堆栈大小
+                (void*          )NULL,                  //传递给任务函数的参数
+                (UBaseType_t    )START_TASK_PRIO,       //任务优先级
+                (TaskHandle_t*  )&StartTask_Handler);   //任务句柄              
+    vTaskStartScheduler();          //开启任务调度
+}
+ 
+//开始任务任务函数
+void start_task(void *pvParameters)
+{
+    taskENTER_CRITICAL();           //进入临界区
+    //创建LED0任务
+    xTaskCreate((TaskFunction_t )led0_task,     	
+                (const char*    )"led0_task",   	
+                (uint16_t       )LED0_STK_SIZE, 
+                (void*          )NULL,				
+                (UBaseType_t    )LED0_TASK_PRIO,	
+                (TaskHandle_t*  )&LED0Task_Handler);   
+    //创建LED1任务
+    xTaskCreate((TaskFunction_t )led1_task,     
+                (const char*    )"led1_task",   
+                (uint16_t       )LED1_STK_SIZE, 
+                (void*          )NULL,
+                (UBaseType_t    )LED1_TASK_PRIO,
+                (TaskHandle_t*  )&LED1Task_Handler);          
+    vTaskDelete(StartTask_Handler); //删除开始任务
+    taskEXIT_CRITICAL();            //退出临界区
+}
+
+//LED0任务函数 
+void led0_task(void *pvParameters)
+{
+    while(1)
+    {
+        GPIO_ToggleBits(GPIOF,GPIO_Pin_9);
+        vTaskDelay(500);
+    }
+}   
+
+//LED1任务函数
+void led1_task(void *pvParameters)
+{
+    while(1)
+    {
+        GPIO_WriteBit(GPIOF,GPIO_Pin_10,Bit_RESET);
+        vTaskDelay(200);
+        GPIO_WriteBit(GPIOF,GPIO_Pin_10,Bit_SET);
+        vTaskDelay(800);
+    }
+}
+
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
