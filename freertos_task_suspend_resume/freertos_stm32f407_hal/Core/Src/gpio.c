5e2db0b29abda66d41c186ebf8781e2687023793
xiaothinking <2681232282@qq.com>
2022-03-30 22:45:14 +0800

    基于STM32F407HAL库生成的FreeRTOS任务的挂起与解挂

---
+++ freertos_task_suspend_resume/freertos_stm32f407_hal/Core/Src/gpio.c
@@ -0,0 +1,130 @@
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+#include "FreeRTOS.h"
+#include "cmsis_os.h"
+#include "usart.h"
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOF, LED0_Pin|LED1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PE2 PE3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PE4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PFPin PFPin */
+  GPIO_InitStruct.Pin = LED0_Pin|LED1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI4_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(EXTI4_IRQn);
+
+}
+
+/* USER CODE BEGIN 2 */
+uint8_t key_sacn(uint8_t mode)
+{
+		static uint8_t key_up=1;//按键按松开标志
+	if(mode)key_up=1;  //支持连按		  
+	if(key_up&&(KEY0==0||KEY1==0||KEY2==0||WK_UP==1))
+	{
+		HAL_Delay(10);//去抖动 
+		key_up=0;
+		if(KEY0==0)return 1;
+		else if(KEY1==0)return 2;
+		else if(KEY2==0)return 3;
+		else if(WK_UP==1)return 4;
+	}
+	else if(KEY0==1&&KEY1==1&&KEY2==1&&WK_UP==0)
+  {
+	  key_up=1;
+	} 	    
+ 	return 0;// 无按键按下
+}
+extern osThreadId myTask04Handle;
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	
+	BaseType_t YieldRequired;
+	if(GPIO_Pin==GPIO_PIN_4)	
+   {
+		YieldRequired=xTaskResumeFromISR(myTask04Handle);//恢复任务2
+		printf("恢复任务2的运行!\r\n");
+		if(YieldRequired==pdTRUE)
+		{
+			/*如果函数xTaskResumeFromISR()返回值为pdTRUE，那么说明要恢复的这个
+			任务的任务优先级等于或者高于正在运行的任务(被中断打断的任务),所以在
+			退出中断的时候一定要进行上下文切换！*/
+			portYIELD_FROM_ISR(YieldRequired);
+		} 
+	 }
+
+
+}
+/* USER CODE END 2 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
