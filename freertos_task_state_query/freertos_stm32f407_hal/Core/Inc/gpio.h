3708934cad06681ffc7dab0813b1e4bc986e40b4
xiaothinking <2681232282@qq.com>
2022-04-05 09:59:06 +0800

    基于STM32F407HAL库生成的FreeRTOS任务状态查询

---
+++ freertos_task_state_query/freertos_stm32f407_hal/Core/Inc/gpio.h
@@ -0,0 +1,52 @@
+/**
+  ******************************************************************************
+  * @file    gpio.h
+  * @brief   This file contains all the function prototypes for
+  *          the gpio.c file
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __GPIO_H__
+#define __GPIO_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN Private defines */
+#define KEY0 		HAL_GPIO_ReadPin(GPIOE,GPIO_PIN_4) //PE4
+#define KEY1 		HAL_GPIO_ReadPin(GPIOE,GPIO_PIN_3)	//PE3 
+#define KEY2 		HAL_GPIO_ReadPin(GPIOE,GPIO_PIN_2) //PE2
+#define WK_UP 	HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_0)	//PA0
+/* USER CODE END Private defines */
+
+void MX_GPIO_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+uint8_t key_sacn(uint8_t mode);
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ GPIO_H__ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ freertos_task_state_query/freertos_stm32f407_hal/Core/Src/freertos.c
@@ -0,0 +1,281 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "usart.h"
+#include "gpio.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+char InfoBuffer[1000];				//保存信息的数组
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+osThreadId defaultTaskHandle;
+osThreadId myTask02Handle;
+osThreadId myTask03Handle;
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+//定义一个测试用的列表和3个列表项
+List_t TestList;		//测试用列表
+ListItem_t ListItem1;	//测试用列表项1
+ListItem_t ListItem2;	//测试用列表项2
+ListItem_t ListItem3;	//测试用列表项3
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void const * argument);
+void StartTask02(void const * argument);
+void StartTask03(void const * argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/* GetIdleTaskMemory prototype (linked to static allocation support) */
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
+
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+  *ppxIdleTaskStackBuffer = &xIdleStack[0];
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  /* place for user code */
+}
+/* USER CODE END GET_IDLE_TASK_MEMORY */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* definition and creation of defaultTask */
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+  /* definition and creation of myTask02 */
+  osThreadDef(myTask02, StartTask02, osPriorityIdle, 0, 128);
+  myTask02Handle = osThreadCreate(osThread(myTask02), NULL);
+
+  /* definition and creation of myTask03 */
+  osThreadDef(myTask03, StartTask03, osPriorityIdle, 0, 128);
+  myTask03Handle = osThreadCreate(osThread(myTask03), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void const * argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_StartTask02 */
+/**
+* @brief Function implementing the myTask02 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask02 */
+void StartTask02(void const * argument)
+{
+  /* USER CODE BEGIN StartTask02 */
+
+  /* Infinite loop */
+  for(;;)
+  {
+    HAL_GPIO_TogglePin(LED0_GPIO_Port,LED0_Pin);		
+    osDelay(500);
+  }
+  /* USER CODE END StartTask02 */
+}
+
+/* USER CODE BEGIN Header_StartTask03 */
+/**
+* @brief Function implementing the myTask03 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask03 */
+void StartTask03(void const * argument)
+{
+  /* USER CODE BEGIN StartTask03 */
+	uint32_t TotalRunTime;
+	UBaseType_t ArraySize,x;
+	TaskStatus_t *StatusArray;
+  /* Infinite loop */
+	//第一步：函数uxTaskGetSystemState()的使用
+	printf("/********第一步：函数uxTaskGetSystemState()的使用**********/\r\n");
+	ArraySize=uxTaskGetNumberOfTasks();		//获取系统任务数量
+	StatusArray=pvPortMalloc(ArraySize*sizeof(TaskStatus_t));//申请内存
+	if(StatusArray!=NULL)					//内存申请成功
+	{
+		ArraySize=uxTaskGetSystemState((TaskStatus_t* 	)StatusArray, 	//任务信息存储数组
+									   (UBaseType_t		)ArraySize, 	//任务信息存储数组大小
+								       (uint32_t*		)&TotalRunTime);//保存系统总的运行时间
+		printf("TaskName\t\tPriority\t\tTaskNumber\t\t\r\n");
+		for(x=0;x<ArraySize;x++)
+		{
+			//通过串口打印出获取到的系统任务的有关信息，比如任务名称、
+			//任务优先级和任务编号。
+			printf("%s\t\t%d\t\t\t%d\t\t\t\r\n",				
+					StatusArray[x].pcTaskName,
+					(int)StatusArray[x].uxCurrentPriority,
+					(int)StatusArray[x].xTaskNumber);
+			
+		}
+	}
+	vPortFree(StatusArray);	//释放内存
+	printf("/**************************结束***************************/\r\n");
+	printf("按下KEY_UP键继续!\r\n\r\n\r\n");
+	while(key_sacn(0)!=4) HAL_Delay(10);		//等待KEY_UP键按下
+	
+	//第二步：函数vTaskGetInfo()的使用
+	TaskHandle_t TaskHandle;	
+	TaskStatus_t TaskStatus;
+	
+	printf("/************第二步：函数vTaskGetInfo()的使用**************/\r\n");
+	TaskHandle=xTaskGetHandle("led0_task");			//根据任务名获取任务句柄。
+	//获取LED0_Task的任务信息
+	vTaskGetInfo((TaskHandle_t	)TaskHandle, 		//任务句柄
+				 (TaskStatus_t*	)&TaskStatus, 		//任务信息结构体
+				 (BaseType_t	)pdTRUE,			//允许统计任务堆栈历史最小剩余大小
+			     (eTaskState	)eInvalid);			//函数自己获取任务运行壮态
+	//通过串口打印出指定任务的有关信息。
+	printf("任务名:                %s\r\n",TaskStatus.pcTaskName);
+	printf("任务编号:              %d\r\n",(int)TaskStatus.xTaskNumber);
+	printf("任务壮态:              %d\r\n",TaskStatus.eCurrentState);
+	printf("任务当前优先级:        %d\r\n",(int)TaskStatus.uxCurrentPriority);
+	printf("任务基优先级:          %d\r\n",(int)TaskStatus.uxBasePriority);
+	printf("任务堆栈基地址:        %#x\r\n",(int)TaskStatus.pxStackBase);
+	printf("任务堆栈历史剩余最小值:%d\r\n",TaskStatus.usStackHighWaterMark);
+	printf("/**************************结束***************************/\r\n");
+	printf("按下KEY_UP键继续!\r\n\r\n\r\n");
+	while(key_sacn(0)!=4) HAL_Delay(10);		//等待KEY_UP键按下
+	
+	//第三步：函数eTaskGetState()的使用	
+	eTaskState TaskState;
+	char TaskInfo[10];
+	printf("/***********第三步：函数eTaskGetState()的使用*************/\r\n");
+	TaskHandle=xTaskGetHandle("query_task");		//根据任务名获取任务句柄。
+	TaskState=eTaskGetState(TaskHandle);			//获取query_task任务的任务壮态
+	memset(TaskInfo,0,10);						
+	switch((int)TaskState)
+	{
+		case 0:
+			sprintf(TaskInfo,"Running");
+			break;
+		case 1:
+			sprintf(TaskInfo,"Ready");
+			break;
+		case 2:
+			sprintf(TaskInfo,"Suspend");
+			break;
+		case 3:
+			sprintf(TaskInfo,"Delete");
+			break;
+		case 4:
+			sprintf(TaskInfo,"Invalid");
+			break;
+	}
+	printf("任务壮态值:%d,对应的壮态为:%s\r\n",TaskState,TaskInfo);
+	printf("/**************************结束**************************/\r\n");
+	printf("按下KEY_UP键继续!\r\n\r\n\r\n");
+	while(key_sacn(0)!=4) HAL_Delay(10);		//等待KEY_UP键按下
+	
+	//第四步：函数vTaskList()的使用	
+	printf("/*************第三步：函数vTaskList()的使用*************/\r\n");
+	vTaskList(InfoBuffer);							//获取所有任务的信息
+	printf("%s\r\n",InfoBuffer);					//通过串口打印所有任务的信息
+	printf("/**************************结束**************************/\r\n");
+	while(1)
+	{
+	  HAL_GPIO_TogglePin(LED1_GPIO_Port,LED1_Pin);	
+    vTaskDelay(1000);                           //延时1s，也就是1000个时钟节拍	
+	}
+  /* USER CODE END StartTask03 */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
